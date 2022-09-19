0a6bd6cefb0df7ec6c4243f4a1211162f9834737
Dominika Bobik <dominikabobik12@gmail.com>
2022-09-19 09:29:15 -0400

    tried fixing form on mobile

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -29,7 +29,7 @@ const Contact: NextPage = () => {
           </div>
 
           <div className={styles.form}>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScyl-4dae6LIakSsJ4mJaBtSbzD1x14pZyaQ_Rmb7gjFe2X7A/viewform?embedded=true" width="100%" height="100%" frameBorder="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScyl-4dae6LIakSsJ4mJaBtSbzD1x14pZyaQ_Rmb7gjFe2X7A/viewform?embedded=true" width="100%" height="100%" frameBorder="0" className={styles.frame}>Loading…</iframe>
           </div>
         </div>
         <Footer />
